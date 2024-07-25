e7bc985434682f7636492546197c993cba241ba9
Isaiah Kantor <isaiahkantor23@gmail.com>
2024-07-25 16:14:59 -0400

    init

---
+++ Tank pygame.py
@@ -0,0 +1,29 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((1000, 800))
+pygame.display.set_caption('Tankzzzz')
+clock = pygame.time.Clock()
+
+ground_surface = pygame.image.load('Graphics/ground1.png').convert()
+sky_surface = pygame.image.load('Graphics/Sky (3) copy.png').convert()
+
+
+
+
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.blit(sky_surface, (0,0))
+    screen.blit(ground_surface, (0,600))
+    
+
+
+    pygame.display.update()
+    clock.tick(60)
