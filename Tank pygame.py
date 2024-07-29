ab75400fae2a26d5089b6b0b5bd10bb0bdcd3944
Joshua Cruz <jcruz1998@gmail.com>
2024-07-29 16:00:35 -0400

    pew

--- Tank pygame.py
+++ Tank pygame.py
@@ -1,5 +1,4 @@
 import pygame
-from lasers import Laser
 from sys import exit
 
 pygame.init()
@@ -10,12 +9,6 @@ clock = pygame.time.Clock()
 ground_surface = pygame.image.load('Graphics/ground1.png').convert()
 sky_surface = pygame.image.load('Graphics/Sky (3) copy.png').convert()
 
-laser = Laser((100, 100))
-laser2 = Laser((100, 200))  
-
-lasers_group = pygame.sprite.Group()
-# Create a group that will hold all the laser beams that the spaceship will fire
-lasers_group.add(laser, laser2)
 
 while True:
     for event in pygame.event.get():
@@ -25,9 +18,6 @@ while True:
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 600))
-
-    lasers_group.draw(screen)  # Draw all lasers in the group
-    lasers_group.update()  # Update all lasers in the group (if any update logic is added later)
 
     pygame.display.update()
     clock.tick(60)
--- lasers.py
+++ lasers.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Laser(pygame.sprite.Sprite):
-    def __init__(self, position):
+    def __init__(self, position,speed, screen_height):
         super().__init__()  # Correct call to the superclass initializer
         self.image = pygame.Surface((4, 15))
         self.image.fill((255, 0, 0))  # This line creates a small rectangular image to represent our laser
@@ -10,3 +10,16 @@ class Laser(pygame.sprite.Sprite):
         # Use the center point to position it. To position the laser, we can achieve this by
         # providing specific coordinates as an argument to the class.
         # These coordinates determine where the laser will appear on the screen when it's first created.
+        self.speed = speed
+        self.screen_height = screen_height 
+
+    def update(self):
+        self.rect.y -= self.speed
+        if self.rect.y > self.screen_height + 15 or self.rect.y <0:
+            self.kill()
+    # to move a laser you just have to reposition its rectangle. Since
+    # can move only in the y direction we just have to add or remove some pixels
+    # to move it up or down. The number of pixels to move per frame will be a class argument 
+    # named speed 
+    # if we want to move the laser up, we will provide a positive value to 
+    # the speed argument. If we want to move the laser down we will provide a negative one
