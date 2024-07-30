99ad0aea9780858683a15461c943d976f57c3ee9
nicole wachter <nicole.wachter6@gmail.com>
2024-07-30 15:59:26 -0400

    adds tank size

--- Tank pygame.py
+++ Tank pygame.py
@@ -1,8 +1,10 @@
 import pygame
 from sys import exit
 
+
 pygame.init()
 screen = pygame.display.set_mode((1000, 800))
+screen_rect = screen.get_rect() # part of tank cant leave screen
 pygame.display.set_caption('Tankzzzz')
 clock = pygame.time.Clock()
 
@@ -14,9 +16,8 @@ class Tank(pygame.sprite.Sprite):
         super().__init__()
         # put all images here for different movements/ put self on everything in the init
         #Define our image
-        img = self.image = pygame.image.load('Graphics/pixil-frame-0.png').convert_alpha()
-        DFLT_IMG_SZ = (200,200)
-        img = pygame.transform.scale(img, DFLT_IMG_SZ)
+        original_image = pygame.image.load('Graphics/green.png').convert_alpha()
+        self.image = pygame.transform.scale(original_image, (300,196))
         # Get rect
         self.rect = self.image.get_rect()
         #Position the image
@@ -28,22 +29,25 @@ class Tank(pygame.sprite.Sprite):
        keys = pygame.key.get_pressed()
        if keys[pygame.K_RIGHT]:
            self.rect.x += 10
-       
+        
        if keys[pygame.K_LEFT]:
            self.rect.x -= 10
 
-
+    def collision(self):
+        # collide_rect(left, right)
+        pass
+       
 #tank instance
-tank = Tank(100, 600)
+tank = Tank(100, 500)
 tank_group = pygame.sprite.Group()
 tank_group.add(tank)
 
-laser = Laser((100, 100))
-laser2 = Laser((100, 200))  
+# laser = Laser((100, 100))
+# laser2 = Laser((100, 200))  
 
 lasers_group = pygame.sprite.Group()
 # Create a group that will hold all the laser beams that the spaceship will fire
-lasers_group.add(laser, laser2)
+# lasers_group.add(laser, laser2)
 
 while True:
     for event in pygame.event.get():
@@ -51,9 +55,9 @@ while True:
             pygame.quit()
             exit()
 
-   
+    tank.rect.clamp_ip(screen_rect) # tank can't leave screen
     tank.move()
-
+     
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 600))
